8743b97ddadf575db4b1251900b926e6a163cc73
vpacifist <v@vovagusev.ru>
2025-10-23 23:00:57 +0200

    feat: add simple words API endpoint

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify
+
 app = Flask(__name__)
+
+WORDS = [
+    {"de": "Haus", "ru": "дом"},
+    {"de": "Baum", "ru": "дерево"},
+    {"de": "Wasser", "ru": "вода"},
+]
 
 @app.route("/")
 def home():
     return "Hello from Railway + Flask!"
+
+@app.route("/api/words")
+def get_words():
+    return jsonify(WORDS)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
