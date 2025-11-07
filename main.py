55aef790ea5fbf099b19f83e1b3029a1759576c5
vpacifist <v@vovagusev.ru>
2025-11-07 04:47:20 +0100

    Fix local server start with socketio.run and eventlet patch

--- main.py
+++ main.py
@@ -150,9 +150,4 @@ def logout():
 
 if __name__ == "__main__":
     init_db()
-    socketio.run(
-        app,
-        host="0.0.0.0",
-        port=8080,
-        debug=False,
-    )
+    socketio.run(app, host="0.0.0.0", port=8080, debug=False, allow_unsafe_werkzeug=True)
